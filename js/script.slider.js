49841e7b2d70e9814d482c6cc5df6a3052ec01ac
ade ade.abidin@gmail.com <ade.abidin@gmail.com>
2016-03-24 22:12:20 +0700

    Slider image sudah jalan

--- js/script.slider.js
+++ js/script.slider.js
@@ -44,3 +44,61 @@
             //slider.reloadSlider();
         }
 })(jQuery); // End of use strict
+
+$('.product-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        margin:20,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3,
+            },
+            1000:{
+                items:5,
+            }
+        }
+    });  
+    
+    $('.related-products-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        margin:20,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:2,
+            },
+            1200:{
+                items:3,
+            }
+        }
+    });  
+    
+    $('.brand-list').owlCarousel({
+        loop:true,
+        nav:true,
+        margin:20,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3,
+            },
+            1000:{
+                items:4,
+            }
+        }
+    });    
+    
