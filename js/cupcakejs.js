283b1faca6f576ceacefb41232a739b14941c661
AdeIrma <ade.abidin@gmail.com>
2016-03-30 20:50:26 -0700

    Finalize ver 3.0 hapus yang gak perlu

--- js/cupcakejs.js
+++ js/cupcakejs.js
@@ -13,16 +13,16 @@
     
     // JQUERY FOR SLIDING NAVIGATION 
 
-	$(document).ready(function() {
-	$('a[href*=#]').each(function() {
-	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-	&& location.hostname == this.hostname
-	&& this.hash.replace(/#/,'') ) {
-	var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-	var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-	if ($target) {
-	var targetOffset = $target.offset().top;
-	});
+	 $(document).ready(function() {
+	 $('a[href*=#]').each(function() {
+	 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+	 && location.hostname == this.hostname
+	 && this.hash.replace(/#/,'') ) {
+	 var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+	 var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+	 if ($target) {
+	 var targetOffset = $target.offset().top;
+	 });
     
     // jQuery Scroll effect
     $('.navbar-nav li a, .scroll-to-up').bind('click', function(event) {
